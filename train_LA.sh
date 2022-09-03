1e9c244a5ac75d7ff230224c3f4dac23357751dd
zxg <zxg@localhost.lan>
2022-09-03 17:44:33 +0800

    test

---
+++ train_LA.sh
@@ -0,0 +1,8 @@
+
+
+python train_LA_8labels.py
+python train_LA_16labels.py
+python train_LA_32labels.py
+python train_LA_64labels.py
+
+
---
+++ train_Liver.sh
@@ -0,0 +1,8 @@
+python train_liver_10labels.py
+python train_liver_20labels.py
+python train_liver_30labels.py
+python train_liver_40labels.py
+python train_liver_50labels.py
+python train_liver_60labels.py
+
+
